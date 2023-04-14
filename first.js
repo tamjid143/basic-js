62764d084bfd1266fb599f2f02b3267fab118d17
tamjid143 <azizulhakimtamjid143@gmail.com>
2023-04-14 09:47:22 +0600

    day second practice turnery

--- first.js
+++ first.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+var a = 10;
+if (a > 10) {
+  console.log("hello");
+} else {
+  if (a < 11) {
+    console.log("from else");
+  } else {
+    console.log("none");
+  }
+}
---
+++ second.js
@@ -0,0 +1,10 @@
+// Turnery Operator
+var a = 10;
+var b = 20;
+var c = a < b ? 100 : 200;
+console.log(c);
+
+var x = 100;
+var y = 300;
+var z = x > y ? 100 : 300;
+console.log(z);
