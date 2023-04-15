5c5b969cc52a794b4dc779242cbc43fdcc6701af
tamjid143 <azizulhakimtamjid143@gmail.com>
2023-04-15 06:41:50 +0600

    another loop practice

---
+++ loop.js
@@ -0,0 +1,18 @@
+// for loop
+for (var i = 1; i < 10; i++) {
+  console.log("step" + i);
+}
+
+// do while / 1st execute loop then check condition
+var num = 1000;
+do {
+  console.log("number" + num);
+  num += 1;
+} while (num < 1010);
+
+// while / 1st check condition then start loop
+var num1 = 1;
+while (num1 < 10) {
+  console.log(num1);
+  num1 += 1;
+}
